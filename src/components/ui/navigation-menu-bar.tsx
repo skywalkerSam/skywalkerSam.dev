98eb08c37618d5a58ba6fbcb2fc3afb8308e933b
Starboy <samskywalker001@gmail.com>
2025-03-23 17:53:26 +0000

    mobile optimization

--- src/components/ui/navigation-menu-bar.tsx
+++ src/components/ui/navigation-menu-bar.tsx
@@ -226,7 +226,7 @@ export function NavigationMenuBar() {
             </NavigationMenuItem>
 
             <NavigationMenuItem>
-              <NavigationMenuTrigger>Project Archives</NavigationMenuTrigger>
+              <NavigationMenuTrigger>Archives</NavigationMenuTrigger>
               <NavigationMenuContent>
                 <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px]">
                   {archivedProjects.map((component) => (
