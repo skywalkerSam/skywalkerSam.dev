fa9af811617a84228b566ee3177a6d079ed49a26
Starboy <samskywalker001@gmail.com>
2025-03-23 22:56:16 +0000

    style optimizations

--- src/components/ui/navigation-menu-bar.tsx
+++ src/components/ui/navigation-menu-bar.tsx
@@ -154,8 +154,8 @@ const archivedProjects: {
 export function NavigationMenuBar() {
   return (
     <div className="flex">
-      <div className="size-14 flex-none"></div>
-      <div className="size-14 flex-grow">
+      <div className="flex size-9 md:flex-none lg:flex-grow"></div>
+      <div className="size-9 flex-grow">
         <NavigationMenu>
           <NavigationMenuList>
             <NavigationMenuItem>
@@ -168,7 +168,12 @@ export function NavigationMenuBar() {
                         className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
                         href="https://github.com/skywalkerSam"
                       >
-                        <img src="/icon.svg" height={60} width={60} alt="Starboy Logo"></img>
+                        <img
+                          src="/icon.svg"
+                          height={60}
+                          width={60}
+                          alt="Starboy Logo"
+                        ></img>
                         <div className="mb-2 mt-4 text-lg font-medium">
                           <p className="text-sm leading-tight text-muted-foreground">
                             Handcrafted by
@@ -254,7 +259,7 @@ export function NavigationMenuBar() {
           </NavigationMenuList>
         </NavigationMenu>
       </div>
-      <div className="size-14 flex-none">
+      <div className="flex size-9 md:flex-none lg:flex-grow">
         <a
           href="https://github.com/skywalkerSam/skywalkerSam.dev"
           target="_blank"
