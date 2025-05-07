1ceded1186132de911b0fc73b46f71c04232dd97
Starboy <samskywalker001@gmail.com>
2025-05-07 01:53:20 +0000

    metadata

--- src/app/about/layout.tsx
+++ src/app/about/layout.tsx
@@ -1,3 +1,13 @@
+import { HOME_OG_IMAGE_URL } from "~/lib/constants";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: `About`,
+  description: `About Starboy...`,
+  openGraph: {
+    images: [HOME_OG_IMAGE_URL],
+  },
+};
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
