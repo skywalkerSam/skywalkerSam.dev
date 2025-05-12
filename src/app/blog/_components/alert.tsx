28963a75c275684acfec8786c0455f55b3d0a914
Starboy <samskywalker001@gmail.com>
2025-05-12 09:32:34 +0530

    repo url fix

--- src/app/blog/_components/alert.tsx
+++ src/app/blog/_components/alert.tsx
@@ -1,5 +1,5 @@
 import Container from "./container";
-import { EXAMPLE_PATH } from "~/lib/constants";
+// import { EXAMPLE_PATH } from "~/lib/constants";
 import cn from "classnames";
 
 type Props = {
@@ -31,7 +31,7 @@ const Alert = ({ preview }: Props) => {
             <>
               The source code for this blog is{" "}
               <a
-                href={`https://github.com/vercel/next.js/tree/canary/examples/${EXAMPLE_PATH}`}
+                href={"https://github.com/skywalkerSam/skywalkerSam.dev"}
                 className="underline hover:text-blue-600 duration-200 transition-colors"
               >
                 available on GitHub
