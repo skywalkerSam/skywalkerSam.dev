af9593ae696258a5e89a3bb4151d634475920d18
Starboy <samskywalker001@gmail.com>
2025-05-12 10:50:13 +0530

    minor fix

--- _data/projects/projects.ts
+++ _data/projects/projects.ts
@@ -4,11 +4,11 @@ export const majorProjects: Projects[] = [
   {
     title: "Project: Stardate",
     href: "https://github.com/skywalkerSam/stardate",
-    description: "A better standard for dating things (at least for me.)",
+    description: "A better way of dating things (at least for me.)",
   },
   {
     title: "Next Gallery",
     href: "https://github.com/skywalkerSam/next-gallery",
-    description: "Personalized Cloud-based Image Gallery w/ NEXT.js + Vercel.",
+    description: "Personalized Cloud-based Image Gallery w/ Next.js + Vercel.",
   },
 ];
