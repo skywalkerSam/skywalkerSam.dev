78b488a31c05e2e72fb5ca85162a6e26f6140eea
Starboy <samskywalker001@gmail.com>
2025-05-13 08:46:35 +0530

    hotfix: link

--- src/app/blog/_components/cover-image.tsx
+++ src/app/blog/_components/cover-image.tsx
@@ -23,7 +23,7 @@ const CoverImage = ({ title, src, slug }: Props) => {
   return (
     <div className="sm:mx-0">
       {slug ? (
-        <Link href={`/posts/${slug}`} aria-label={title}>
+        <Link href={`/blog/posts/${slug}`} aria-label={title}>
           {image}
         </Link>
       ) : (
