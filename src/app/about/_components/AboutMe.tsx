792395e7ba385e20fd113e782bc0956ecb766bb2
Starboy <samskywalker001@gmail.com>
2025-07-17 10:17:32 +0000

    about

--- src/app/about/_components/AboutMe.tsx
+++ src/app/about/_components/AboutMe.tsx
@@ -2,7 +2,7 @@ export default function AboutMe() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center p-4">
       <p className="mx-auto max-w-3xl text-lg md:max-w-4xl md:text-xl md:tracking-wide lg:text-2xl">
-        A wise man once said: <span className="text-sky-400">"</span>To define
+        A wise man once said, <span className="text-sky-400">"</span>To define
         is to limit.<span className="text-sky-400">"</span> Nevertheless
         <span className="text-sky-400">,</span> here we go
         <span className="text-sky-400">...</span> i
@@ -12,7 +12,7 @@ export default function AboutMe() {
         Ubuntu on my ancient PC that was having a hard time with Windows
         <span className="text-sky-400">.</span> i started learning more and more
         <span className="text-sky-400">,</span> and life went on and on
-        <span className="text-sky-400">...</span>
+        <span className="text-sky-400">... </span>
         and here i am<span className="text-sky-400">.</span> What began as a
         hobby turned into a kind of passion: Linux
         <span className="text-sky-400">,</span> BA$H Scripts
