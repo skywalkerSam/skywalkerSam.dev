d4955e89f2062ac4effa73a142165c63d71f24fe
Starboy <samskywalker001@gmail.com>
2025-07-17 10:13:45 +0000

    about

---
+++ src/app/about/_components/animated-intro.tsx
@@ -0,0 +1,61 @@
+"use client";
+import "./animated-intro.css";
+import { nameList, doList, emojiList } from "_data/constants";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
+export default function AnimatedIntro() {
+  useGSAP(() => {
+    gsap.fromTo(
+      ".hero-text h1",
+      { y: 30, opacity: 0 },
+      { y: 0, opacity: 1, stagger: 0.2, duration: 1, ease: "power2.inOut" },
+    );
+  });
+  return (
+    <div className="flex min-h-screen px-36">
+      <div className="hero-text">
+        <h1>
+          <span className="slide">
+            <span className="wrapper">
+              {emojiList.map((word, i) => (
+                <span key={i} className="flex items-center gap-1 pb-2 md:gap-3">
+                  <span>{word.text}</span>
+                </span>
+              ))}
+            </span>
+          </span>
+        </h1>
+        <br />
+        <h1>
+          i<span className="text-sky-400">&apos;</span>m
+          <span className="slide">
+            <span className="wrapper">
+              {nameList.map((word, i) => (
+                <span key={i} className="flex items-center gap-1 pb-2 md:gap-3">
+                  <span className="text-sky-400">{word.text}</span>
+                </span>
+              ))}
+            </span>
+          </span>
+        </h1>
+        <h1>
+          i
+          <span className="slide">
+            <span className="wrapper">
+              {doList.map((word, i) => (
+                <span key={i} className="flex items-center gap-1 pb-2 md:gap-3">
+                  
+                  <span className="text-sky-400">{word.text}</span>
+                </span>
+              ))}
+            </span>
+          </span>
+        </h1>
+        <h1>
+          Stuff<span className="text-sky-400">.</span>
+        </h1>
+      </div>
+    </div>
+  );
+}
---
+++ src/app/about/_components/intro.tsx
@@ -0,0 +1,33 @@
+"use client";
+import Image from "next/image";
+
+export default function Intro() {
+  return (
+    <div className="min-h-screen bg-gradient-to-b from-transparent via-blue-400 to-transparent p-4">
+      <div className="flex flex-col items-center justify-center">
+        <div className="mt-36">
+          <h1 className="py-9 text-xl md:text-3xl lg:text-4xl">
+            👋 i'm Sam, i write and build things...
+          </h1>
+        </div>
+        <div className="w-48 py-4 md:w-60 lg:w-72">
+          <Image
+            src="https://github.com/skywalkerSam.png"
+            width={300}
+            height={300}
+            alt="Profile Image"
+          ></Image>
+
+          <a
+            href="https://twitter.com/skywalkerSam_"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex items-end justify-end text-xs hover:text-sky-400"
+          >
+            @skywalkerSam_
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
