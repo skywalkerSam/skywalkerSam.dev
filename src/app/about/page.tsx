c3a48f54b426008a48586de7e5cf7cf491b09229
Starboy <samskywalker001@gmail.com>
2025-08-30 08:41:40 +0000

    ISR

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,11 +4,14 @@ import AnimatedIntro from "./_components/animated-intro";
 import Bio from "./_components/Bio";
 import Footer from "~/components/Footer";
 
+// ISR
+export const revalidate = 3600;
+
 export default function About() {
   return (
     <div className="relative h-screen w-screen">
       <Stars></Stars>
-      <div className="relative z-1">
+      <div className="z-1 relative">
         <AnimatedIntro></AnimatedIntro>
         <Bio></Bio>
         <AboutMe></AboutMe>
